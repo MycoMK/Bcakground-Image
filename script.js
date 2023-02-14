00dcc0024cc549940e1d18d05c87b2a565246138
Myco <kmichael831@gmail.com>
2023-02-14 09:58:51 -0800

    edits

---
+++ script.js
@@ -0,0 +1,10 @@
+let myDiv = document.querySelector('div');
+
+myDiv.addEventListener('mouseover',()=>{
+    body.style.backgroundImage = `url('2.jpeg')`;
+})
+
+myDiv.addEventListener('mouseover',()=>{
+    body.style.backgroundImage = `url('images/1.jpeg')`;
+})
+
